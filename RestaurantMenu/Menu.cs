47e9d00913be8d565257663ffca6516b38df73e6
Kiaran <klearyire@gmail.com>
2021-09-30 20:54:05 -0400

    Add project files.

---
+++ RestaurantMenu/Menu.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantMenu
+{
+    public class Menu
+    {
+
+        private bool NewItem { get; set; }
+        private List<MenuItems> MenuItems { get; set; }
+
+        private DateTime Date1 = new DateTime();
+
+        public Menu(bool newItem, List<MenuItems> menuItems, DateTime date1)
+        {
+            NewItem = newItem;
+            MenuItems = menuItems;
+            Date1 = date1;
+        }
+    }
+}
---
+++ RestaurantMenu/MenuItems.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantMenu
+{
+    public class MenuItems
+    {
+        private string ItemName { get; set; }
+        private string Description { get; set; }
+        private double Price { get; set; }
+        private string Category { get; set; }
+
+        public MenuItems(string itemName, string description, double price, string category)
+        {
+            ItemName = itemName;
+            Description = description;
+            Price = price;
+            Category = category;
+        }
+    }
+}
